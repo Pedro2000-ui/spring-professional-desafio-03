79dddd83f2deba8e1cdaaf97636eacba5ae036c8
Pedro Henrique <pholiveirabrito@hotmail.com>
2026-09-09 10:24:37 -0300

    feat: endpoint para listar os clients paginados

--- src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.desafio3.controllers;
 import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +24,10 @@ public class ClientController {
         return dto;
     }
 
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable page)
+    {
+        Page<ClientDTO> dto = clientService.findAll(page);
+        return dto;
+    }
 }
--- src/main/java/com/devsuperior/desafio3/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/services/ClientService.java
@@ -4,6 +4,8 @@ import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.entities.Client;
 import com.devsuperior.desafio3.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,13 @@ public class ClientService {
     {
         Client client = clientRepository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable page)
+    {
+        Page<Client> clients = clientRepository.findAll(page);
+        return clients.map(ClientDTO::new);
     }
 
 }
